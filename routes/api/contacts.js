58edc1acbf52a001e89e62610ccc4dce42bcc196
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-09-19 10:46:39 +0300

    Amends contacts routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../../controllers/contactsControllers");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const schemas = require("../../utils/validation/contactValidationSchemas");
 
-router.get("/", controllers.getAll);
-router.get("/:contactId", isValidId, controllers.getById);
-router.post("/", validateBody(schemas.addContactValidationSchema), controllers.addContact);
-router.delete("/:contactId", isValidId, controllers.removeContact);
+router.get("/", authenticate, controllers.getAll);
+router.get("/:contactId", authenticate, isValidId, controllers.getById);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addContactValidationSchema),
+  controllers.addContact
+);
+router.delete("/:contactId", authenticate, isValidId, controllers.removeContact);
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.addContactValidationSchema),
   controllers.updateContact
 );
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   controllers.updateContact
